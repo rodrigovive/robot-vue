000f0311d01b32131ad72454b5478d51bc7118be
Rodrigo <cherry199662@gmail.com>
2018-08-18 11:20:43 -0500

    Header,HomePage add

---
+++ src/components/HomePage.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="home">
+    <div>
+      <img class="robot" src="../assets/robot-home.png" />
+    </div>
+    <div class="get-started">
+      <a href="">Get started</a> contruye tu primer robot!
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .home {
+    text-align: center;
+  }
+  .robot {
+    heigth: 300px;
+  }
+</style>

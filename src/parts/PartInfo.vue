da7ae69097e23ddd4e8b97981b1e79c8aa48f9bf
Rodrigo <cherry199662@gmail.com>
2018-08-20 09:46:38 -0500

    Adding validator for part route

--- src/parts/PartInfo.vue
+++ src/parts/PartInfo.vue
@@ -1,23 +1,36 @@
 <template>
     <div>
-        <h1>{{part.title}}</h1>
+        <h1>{{parts.title}}</h1>
         <div>
-            {{part.description}}
+            {{parts.description}}
         </div>
     </div>
 </template>
 
 <script>
+import parts from '../data/parts';
+
 export default {
   name: 'PartInfo',
-  data() {
-    return {
-      part: {
-        title: 'Part Title',
-        description: 'Part Description',
+  props: {
+    partType: {
+      type: String,
+    },
+    id: {
+      type: [Number, String],
+      validator(value) {
+        return Number.isInteger(Number(value));
       },
-    };
+    },
   },
+  computed: {
+    parts() {
+      const { partType, id } = this;
+
+      return parts[partType].find(part => part.id === +id);
+    },
+  },
+
 };
 </script>
 

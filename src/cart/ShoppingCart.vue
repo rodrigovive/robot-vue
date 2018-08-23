b42ae104016bc7fa969f2ae0d6dde67665981ac7
Rodrigo <cherry199662@gmail.com>
2018-08-22 21:43:18 -0500

    Adding getters on Vuex

--- src/cart/ShoppingCart.vue
+++ src/cart/ShoppingCart.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <h1>Cart</h1>
+        <h2>Cart</h2>
         <table>
             <thead>
             <tr>
@@ -23,6 +23,29 @@
             </tr>
             </tbody>
         </table>
+        <h2 class="saleItems">You saved money on these robots:</h2>
+        <table>
+            <thead>
+            <tr>
+                <th class="robot-title">
+                    Robot
+                </th>
+                <th class="cost">
+                    Cost
+                </th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr v-for="(robot, index) in cartSaleItems" :key="index">
+                <td class="robot-title">
+                    {{robot.head.title}}
+                </td>
+                <td class="cost">
+                    {{robot.cost}}
+                </td>
+            </tr>
+            </tbody>
+        </table>
     </div>
 </template>
 
@@ -32,8 +55,11 @@ export default {
   computed: {
     cart() {
       return this.$store.state.cart;
-    }
-  }
+    },
+    cartSaleItems() {
+      return this.$store.getters.cartSaleItems;
+    },
+  },
 };
 </script>
 
@@ -47,5 +73,10 @@ export default {
     }
     .cost {
         text-align: right;
+    }
+    .saleItems {
+        margin-top: 50px;
+        font-size: 18px;
+        color: red;
     }
 </style>
